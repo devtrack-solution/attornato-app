53380c13cd2aad2aed034c0d88ab4c61e6a9be0b
Laercio <laercio.aaavila@gmail.com>
2025-05-21 09:42:16 -0300

    improvements for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-export AWS_DEFAULT_PROFILE=attornato
 export AWS_PROFILE=attornato
-export AWS_S3_REPOSITORY=s3://attornato-site-client-repository
+export REPOSITORY=s3://attornato-app-client-repository
+export DISTRIBUTION=E140W0IJS8BQ2O
 
-aws s3 ls $AWS_S3_REPOSITORY --profile $AWS_PROFILE
+# Validação e limpeza
+aws s3 ls $REPOSITORY --profile $AWS_PROFILE
+aws s3 rm $REPOSITORY --recursive --profile $AWS_PROFILE
 
-aws s3 rm $AWS_S3_REPOSITORY --recursive --profile $AWS_PROFILE
+# Envio correto do build
+aws s3 sync ./dist/diamond-ng $REPOSITORY --profile $AWS_PROFILE
 
-aws s3 sync ./dist $AWS_S3_REPOSITORY --profile $AWS_PROFILE
+# Corrige Content-Type de arquivos JS
+aws s3 sync ./dist/diamond-ng $REPOSITORY --exclude "*" --include "*.js" --content-type "application/javascript" --profile $AWS_PROFILE
 
-aws cloudfront create-invalidation --distribution-id E17W96AC4K1K51 --paths "/*" --profile $AWS_PROFILE
+# Invalidação do cache do CloudFront
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths "/*" --profile $AWS_PROFILE
